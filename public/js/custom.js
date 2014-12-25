f40a86e8ac7426ebc53d3c52c953250a001b3b89
bchoi12 <brian.choi222@gmail.com>
2014-12-25 10:13:39 -0600

    test

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,8 +8,12 @@ $(window).scroll(function() {
 	if ( 2 * off < $(window).scrollTop() ) {
 		$('.navbar-wrapper').css('position', 'fixed');
 		$('.navbar-wrapper').css('width', '100%');
+		$('.navbar-wrapper').css('background-color', '#222');
+		$('.navbar').css('margin-bottom', '0');
 	} else if (off > $(window).scrollTop() ) {
 		$('.navbar-wrapper').css('position', 'absolute');
 		$('.navbar-wrapper').css('width', 'auto');
+		$('.navbar-wrapper').css('background-color', 'transparent');
+		$('.navbar').css('margin-bottom', '20px');
 	}
 });
