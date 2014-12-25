0996b6d5b446c4a90bdadf5b347566e6fe666e66
bchoi12 <brian.choi222@gmail.com>
2014-12-25 10:29:02 -0600

    test

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,21 +5,12 @@ $(document).ready(function() {
 });
 
 var navHeight = $('.navbar-wrapper').outerHeight();
-var navWidth = $('.navbar-anim').outerWidth();
 
-$(window).resize(function() {
-	navHeight = $('.navbar-wrapper').outerHeight();
-	navWidth = $('.navbar-anim').outerWidth();	
-});
 
 $(window).scroll(function() {
 	if ( 2 * navHeight < $(window).scrollTop() ) {
-		$('.navbar-wrapper .navbar-anim').css('background-color', '#222');
-		$('.navbar-anim').css('width', '100%');
-		$('.navbar').css('margin-bottom', '0');
+		$('.navbar').css('padding-bottom', '0');
 	} else if (navHeight > $(window).scrollTop() ) {
-		$('.navbar-wrapper .navbar-anim').css('background-color', 'transparent');
-		$('.navbar-anim').css('width', navWidth + 'px');
-		$('.navbar').css('margin-bottom', '10px');
+		$('.navbar').css('padding-bottom', '10px');
 	}
 });
