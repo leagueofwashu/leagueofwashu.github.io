371be174012d617956b120d4ecba792c7b2e78c2
bchoi12 <brian.choi222@gmail.com>
2014-12-26 06:06:13 -0600

    quick fix

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
 
 $(window).scroll(function() {
 	if ( $(window).scrollTop() > 30 && $('.navbar-toggle').css('display') === 'none') {
-		$('.navbar-wrapper').css('opacity', '1');
+		$('.navbar-wrapper').css('opacity', '0.8');
 		$('.navbar-brand').css('height', 'auto');
 		$('.navbar-brand').css('padding-top', '8px');
 		$('.navbar-brand').css('padding-bottom', '8px');
 		$('.navbar-nav>li>a').css('padding-top', '8px');
 		$('.navbar-nav>li>a').css('padding-bottom', '8px');
 	} else {
-		$('.navbar-wrapper').css('opacity', '0.77');
+		$('.navbar-wrapper').css('opacity', '1');
 		$('.navbar-brand').css('height', '40px');
 		$('.navbar-brand').css('padding-top', '15px');
 		$('.navbar-brand').css('padding-bottom', '15px');
