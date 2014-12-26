9708b36aa4062fa41b393aef77e8ec5bc07842d4
bchoi12 <brian.choi222@gmail.com>
2014-12-25 22:00:08 -0600

    navbar

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
 	console.log('hello');
+	$('.navbar').css('min-height', '40px');
 });
 
 
@@ -9,8 +10,16 @@ $(window).scroll(function() {
 	if ( $(window).scrollTop() > 10 ) {
 		$('.navbar').css('padding-bottom', '0');
 		$('.navbar').css('padding-top', '0');
+		$('.navbar-brand').css('height', 'auto');
+		$('.navbar-brand').css('padding', '10px');
+		$('.navbar-nav>li>a').css('padding-top', '5px');
+		$('.navbar-nav>li>a').css('padding-bottom', '5px');
 	} else {
 		$('.navbar').css('padding-bottom', '7px');
-		$('.navbar').css('padding-top', '3px');
+		$('.navbar').css('padding-top', '7px');
+		$('.navbar-brand').css('height', '50px');
+		$('.navbar-brand').css('padding', '15px');
+		$('.navbar-nav>li>a').css('padding-top', '10px');
+		$('.navbar-nav>li>a').css('padding-bottom', '10px');
 	}
 });
