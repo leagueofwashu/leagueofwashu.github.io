aa3de58d708e4c086cbf4ded97601f9d75d7c389
bchoi12 <brian.choi222@gmail.com>
2014-12-25 22:04:37 -0600

    navbar 2

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,18 +8,16 @@ $(document).ready(function() {
 
 $(window).scroll(function() {
 	if ( $(window).scrollTop() > 10 ) {
-		$('.navbar').css('padding-bottom', '0');
 		$('.navbar').css('padding-top', '0');
 		$('.navbar-brand').css('height', 'auto');
 		$('.navbar-brand').css('padding', '10px');
-		$('.navbar-nav>li>a').css('padding-top', '5px');
-		$('.navbar-nav>li>a').css('padding-bottom', '5px');
-	} else {
-		$('.navbar').css('padding-bottom', '7px');
-		$('.navbar').css('padding-top', '7px');
-		$('.navbar-brand').css('height', '50px');
-		$('.navbar-brand').css('padding', '15px');
 		$('.navbar-nav>li>a').css('padding-top', '10px');
 		$('.navbar-nav>li>a').css('padding-bottom', '10px');
+	} else {
+		$('.navbar').css('padding-top', '15px');
+		$('.navbar-brand').css('height', '40px');
+		$('.navbar-brand').css('padding', '15px');
+		$('.navbar-nav>li>a').css('padding-top', '15px');
+		$('.navbar-nav>li>a').css('padding-bottom', '15px');
 	}
 });
